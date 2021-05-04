8caf174e6f5776f020eaff9887c7b130605159a8
Laturkar (EXT) <trhae7@splm.siemens.com>
2021-05-04 16:57:23 +0530

    Added logger statements

--- src/main/java/com/siemens/mindsphere/controllers/BillboardController.java
+++ src/main/java/com/siemens/mindsphere/controllers/BillboardController.java
@@ -17,7 +17,10 @@ import com.siemens.mindsphere.sdk.core.exception.MindsphereException;
 import com.siemens.mindsphere.services.BillboardService;
 import com.siemens.mindsphere.services.MindsphereService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class BillboardController {
 	/**
 	 * This controller class is used to handle token related operations : toggle token, get token.
@@ -29,17 +32,21 @@ public class BillboardController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/tokenType/toggle")
     public String toggle() {
-
+    	log.info("/tokenType/toggle endpoint invoked");
         MindsphereService.presentTokenType = MindsphereService.getNextTokenType();
+        log.info("The token type is changed to : " + MindsphereService.presentTokenType);
         return "The token type is changed to : " + MindsphereService.presentTokenType;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/token")
     public List<String> token(@RequestHeader(required = false, value = "Authorization") String userToken,
             HttpServletRequest request) throws MindsphereException {
+    	 log.info("/token endpoint invoked");
         if(MindsphereService.presentTokenType.equals(MindsphereService.TokenType.USER) && userToken!=null) {
             billboardService.getUserToken(userToken);
+            log.info("User Token returned");
         }else if(MindsphereService.presentTokenType.equals(MindsphereService.TokenType.APP)) {
+        	log.info("App Token returned");
             return billboardService.getTechnicalToken(request.getRequestURL().toString());
         }
         return null;
@@ -48,9 +55,12 @@ public class BillboardController {
     @RequestMapping(method = RequestMethod.GET, value = "/jwt")
     public String jwt(@RequestHeader(required = false, value = "Authorization") String userToken,
             HttpServletRequest request) throws MindsphereException {
+    	log.info("/jwt endpoint invoked");
         if(MindsphereService.presentTokenType.equals(MindsphereService.TokenType.USER) && userToken!=null) {
+        	 log.info("User Token returned");
             return userToken;
         }else if(MindsphereService.presentTokenType.equals(MindsphereService.TokenType.APP)) {
+        	log.info("App Token returned");
             return billboardService.getEnCodedToken(request.getRequestURL().toString());
         }
         return null;
